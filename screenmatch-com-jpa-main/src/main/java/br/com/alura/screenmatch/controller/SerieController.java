c36da0bc09cb92a8dd6be200ffbba4c9a8c4e088
Lucas <lucasfiguero2000@gmail.com>
2024-05-04 16:07:35 -0300

    Adicionando rotas e classe DTO para a série, arrumando o erro de CORS

--- screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,8 +1,32 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
-    
+
+    @Autowired
+    private SerieRepository repository;
+
+    @GetMapping("/series")
+    public List<SerieDTO> obterSeries() {
+        return repository.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
+                s.getGenero(), s.getAtores(), s.getImagemPoster(), s.getSinopse())).toList();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5() {
+        return repository.findTop5ByOrderByAvaliacaoDesc()
+                .stream()
+                .map(serie -> new SerieDTO(serie.getId(), serie.getTitulo(), serie.getTotalTemporadas(), serie.getAvaliacao(),
+                        serie.getGenero(), serie.getAtores(), serie.getImagemPoster(), serie.getSinopse())).toList();
+    }
 }
---
+++ screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/dto/SerieDTO.java
@@ -0,0 +1,16 @@
+package br.com.alura.screenmatch.dto;
+
+import br.com.alura.screenmatch.model.Categoria;
+
+//Aqui eu decido que campos vou distribuir para o frontend.
+public record SerieDTO(
+        Long id,
+        String titulo,
+        Integer totalTemporadas,
+        Double avaliacao,
+        Categoria genero,
+        String atores,
+        String imagemPoster,
+        String sinopse
+) {
+}
